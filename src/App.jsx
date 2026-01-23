29aa1ce771751c07aff598a85166f044476f527c
Jose Antonio Verano <mjaverano@tip.edu.ph>
2026-01-23 22:11:21 +0800

    Add conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import UserList from './UserList';
-import Counter from './Counter'; // Import Counter
+import Counter from './Counter';
 
 function App() {
   const users = ['Alice', 'Bob', 'Charlie'];
+  const isLoggedIn = true; // Try changing this to false later!
 
   return (
     <div>
       <h1>Lab Activity 3</h1>
+      {/* Conditional Rendering */}
+      {isLoggedIn ? <p>Welcome back, User!</p> : <p>Please log in.</p>}
+
       <UserList users={users} />
       <Counter />
     </div>
